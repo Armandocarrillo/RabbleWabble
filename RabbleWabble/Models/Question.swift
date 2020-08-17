880d86aee60ec216c55d4e73cebaed2f6b10b0c1
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-17 12:52:15 -0500

    Creating the models and add LaunchScreen

---
+++ RabbleWabble/Models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Armando Carrillo on 17/08/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer : String
+    public let hint : String?
+    public let prompt : String
+}
---
+++ RabbleWabble/Models/QuestionsGroups.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionsGroups.swift
+//  RabbleWabble
+//
+//  Created by Armando Carrillo on 17/08/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions : [Question]
+    public let title : String
+}
