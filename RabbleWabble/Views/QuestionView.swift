1bece68ff0d49dcc7e23c2c4afbe9a8287352b2c
Armando Carrillo <ingcompuarmando@gmail.com>
2020-08-17 14:37:49 -0500

    Creating the view

---
+++ RabbleWabble/Views/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  RabbleWabble
+//
+//  Created by Armando Carrillo on 17/08/20.
+//  Copyright © 2020 Armando Carrillo. All rights reserved.
+//
+
+import UIKit
+
+public class QuestionView: UIView {
+    @IBOutlet public var answerLabel: UILabel!
+    @IBOutlet public var correctCountLabel : UILabel!
+    @IBOutlet public var incorrectCountLabel : UILabel!
+    @IBOutlet public var promptLabel : UILabel!
+    @IBOutlet public var hintLabel : UILabel!
+    
+}
